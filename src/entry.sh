9e5c10a4ca12b978a48a3038f88455e3c807e73a
Julian Pawlowski <julian@pawlowski.me>
2019-01-28 13:17:01 +0100

    add permissions for USB devices

--- src/entry.sh
+++ src/entry.sh
@@ -125,8 +125,14 @@ echo "$i. Enforcing user and group ownership for ${FHEM_DIR} to fhem:fhem ..."
 chown --recursive --quiet --no-dereference ${FHEM_UID}:${FHEM_GID} ${FHEM_DIR}/ 2>&1>/dev/null
 (( i++ ))
 echo "$i. Correcting group ownership for /dev/tty* ..."
-[ -e /dev/tty ] && chown --recursive --quiet --no-dereference .tty /dev/tty* 2>&1>/dev/null && chmod --recursive --quiet g+rw /dev/tty* 2>&1>/dev/null
-[ -e /dev/ttyS0 ] && chown --recursive --quiet --no-dereference .dialout /dev/ttyS* 2>&1>/dev/null && chmod --recursive --quiet g+rw /dev/ttyS* 2>&1>/dev/null
+#find /dev/ -name "tty*" -exec chown --recursive --quiet --no-dereference .tty {} \;
+find /dev/ -name "ttyS*" -exec chown --recursive --quiet --no-dereference .dialout {} \;
+find /dev/ -name "ttyACM*" -exec chown --recursive --quiet --no-dereference .dialout {} \;
+find /dev/ -name "ttyUSB*" -exec chown --recursive --quiet --no-dereference .dialout {} \;
+#find /dev/ -name "tty*" -exec chmod --recursive --quiet g+w {} \;
+find /dev/ -name "ttyS*" -exec chmod --recursive --quiet g+rw {} \;
+find /dev/ -name "ttyACM*" -exec chmod --recursive --quiet g+rw {} \;
+find /dev/ -name "ttyUSB*" -exec chmod --recursive --quiet g+rw {} \;
 (( i++ ))
 if [[ "$(find /dev/ -name "gpio*")" -ne "" || -d /sys/devices/virtual/gpio || -d /sys/devices/platform/gpio-sunxi/gpio || /sys/class/gpio ]]; then
   echo "$i. Found GPIO: Correcting group permissions in /dev and /sys to 'gpio' with GID ${GPIO_GID} ..."
