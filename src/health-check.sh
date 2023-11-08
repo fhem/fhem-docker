e87fbabbb6aac3cb2d4fb5cf6d0023b96df53f5e
sidey79 <7968127+sidey79@users.noreply.github.com>
2023-11-08 22:53:19 +0100

    fix[health-check]: Newlines fixed

--- src/health-check.sh
+++ src/health-check.sh
@@ -1,5 +1,240 @@
 #!/bin/bash
 
+
+#--- Constants -------------------------------------------------------------------------------------------------------
+
+declare -r PID_FILE="/var/run/health-check.pid"
+declare -r URL_FILE="/tmp/health-check.urls"
+declare -r RESULT_FILE="/tmp/health-check.result"
+
+
+#--- Internal global -------------------------------------------------------------------------------------------------
+
+declare -i gRetVal=0;
+declare    gRetMessage="";
+declare -i gSuccessCnt=0;
+declare -i gFailedCnt=0;
+
+
+#====================================================================================================================-
+#--- Functions -------------------------------------------------------------------------------------------------------
+
+# Handler function for actually stopping this script. Called either by
+#  - "exit" was called somewhere in the script
+#  - SIGTERM is received
+#
+# Usage: trapExitHandler
+# Global vars: gRetMessage
+#              gSuccessCnt
+#              gFailedCnt
+#              RESULT_FILE
+#              PID_FILE
+#
+function trapExitHandler() {
+  local -i exitVal=$?  # when "exit" was called, this holds the return value
+  trap - SIGTERM EXIT  # Avoid multiple calls to handler
+  echo "$gRetMessage"
+  if (($exitVal == 0)) ; then
+    echo -n "ok ($gSuccessCnt successful,  $gFailedCnt failed)" > $RESULT_FILE
+  else
+    echo -n "ERROR ($gSuccessCnt successful,  $gFailedCnt failed)" > $RESULT_FILE
+  fi
+  rm -f $PID_FILE
+  exit $exitVal
+}
+
+
+#====================================================================================================================-
+#--- Main script -----------------------------------------------------------------------------------------------------
+
+[ -e $PID_FILE ] && { echo "Instance already running, aborting another one" ; exit 1; } # run before installing traphandler!
+trap trapExitHandler SIGTERM EXIT
+
+echo "$$" > $PID_FILE
+
+[ -e $URL_FILE ] || { gRetMessage="Cannot read url file $URL_FILE" ; exit 1; }
+
+while IFS= read -r fhemUrl; do
+  fhemwebState=$( curl \
+                    --silent \
+                    --insecure \
+                    --output /dev/null \
+                    --write-out "%{http_code}" \
+                    --user-agent 'FHEM-Docker/1.0 Health Check' \
+                    "${fhemUrl}" )
+  if [ $? -ne 0 ] ||
+     [ -z "${fhemwebState}" ] ||
+     [ "${fhemwebState}" == "000" ] ||
+     [ "${fhemwebState:0:1}" == "5" ]; then
+    gRetMessage="$gRetMessage $fhemUrl: FAILED ($fhemwebState);"
+    gRetVal=1
+    ((gFailedCnt++))
+  else
+    gRetMessage="$gRetMessage $fhemUrl: OK;"
+    ((gSuccessCnt++))
+  fi
+done < $URL_FILE
+
+exit $gRetVal
+
+#!/bin/bash
+
+#--- Constants -------------------------------------------------------------------------------------------------------
+
+declare -r PID_FILE="/var/run/health-check.pid"
+declare -r URL_FILE="/tmp/health-check.urls"
+declare -r RESULT_FILE="/tmp/health-check.result"
+
+
+#--- Internal global -------------------------------------------------------------------------------------------------
+
+declare -i gRetVal=0;
+declare    gRetMessage="";
+declare -i gSuccessCnt=0;
+declare -i gFailedCnt=0;
+
+
+#====================================================================================================================-
+#--- Functions -------------------------------------------------------------------------------------------------------
+
+# Handler function for actually stopping this script. Called either by
+#  - "exit" was called somewhere in the script
+#  - SIGTERM is received
+#
+# Usage: trapExitHandler
+# Global vars: gRetMessage
+#              gSuccessCnt
+#              gFailedCnt
+#              RESULT_FILE
+#              PID_FILE
+#
+function trapExitHandler() {
+  local -i exitVal=$?  # when "exit" was called, this holds the return value
+  trap - SIGTERM EXIT  # Avoid multiple calls to handler
+  echo "$gRetMessage"
+  if (($exitVal == 0)) ; then
+    echo -n "ok ($gSuccessCnt successful,  $gFailedCnt failed)" > $RESULT_FILE
+  else
+    echo -n "ERROR ($gSuccessCnt successful,  $gFailedCnt failed)" > $RESULT_FILE
+  fi
+  rm -f $PID_FILE
+  exit $exitVal
+}
+
+
+#====================================================================================================================-
+#--- Main script -----------------------------------------------------------------------------------------------------
+
+[ -e $PID_FILE ] && { echo "Instance already running, aborting another one" ; exit 1; } # run before installing traphandler!
+trap trapExitHandler SIGTERM EXIT
+
+echo "$$" > $PID_FILE
+
+[ -e $URL_FILE ] || { gRetMessage="Cannot read url file $URL_FILE" ; exit 1; }
+
+while IFS= read -r fhemUrl; do
+  fhemwebState=$( curl \
+                    --silent \
+                    --insecure \
+                    --output /dev/null \
+                    --write-out "%{http_code}" \
+                    --user-agent 'FHEM-Docker/1.0 Health Check' \
+                    "${fhemUrl}" )
+  if [ $? -ne 0 ] ||
+     [ -z "${fhemwebState}" ] ||
+     [ "${fhemwebState}" == "000" ] ||
+     [ "${fhemwebState:0:1}" == "5" ]; then
+    gRetMessage="$gRetMessage $fhemUrl: FAILED ($fhemwebState);"
+    gRetVal=1
+    ((gFailedCnt++))
+  else
+    gRetMessage="$gRetMessage $fhemUrl: OK;"
+    ((gSuccessCnt++))
+  fi
+done < $URL_FILE
+
+exit $gRetVal
+
+#!/bin/bash
+
+#--- Constants -------------------------------------------------------------------------------------------------------
+
+declare -r PID_FILE="/var/run/health-check.pid"
+declare -r URL_FILE="/tmp/health-check.urls"
+declare -r RESULT_FILE="/tmp/health-check.result"
+
+
+#--- Internal global -------------------------------------------------------------------------------------------------
+
+declare -i gRetVal=0;
+declare    gRetMessage="";
+declare -i gSuccessCnt=0;
+declare -i gFailedCnt=0;
+
+
+#====================================================================================================================-
+#--- Functions -------------------------------------------------------------------------------------------------------
+
+# Handler function for actually stopping this script. Called either by
+#  - "exit" was called somewhere in the script
+#  - SIGTERM is received
+#
+# Usage: trapExitHandler
+# Global vars: gRetMessage
+#              gSuccessCnt
+#              gFailedCnt
+#              RESULT_FILE
+#              PID_FILE
+#
+function trapExitHandler() {
+  local -i exitVal=$?  # when "exit" was called, this holds the return value
+  trap - SIGTERM EXIT  # Avoid multiple calls to handler
+  echo "$gRetMessage"
+  if (($exitVal == 0)) ; then
+    echo -n "ok ($gSuccessCnt successful,  $gFailedCnt failed)" > $RESULT_FILE
+  else
+    echo -n "ERROR ($gSuccessCnt successful,  $gFailedCnt failed)" > $RESULT_FILE
+  fi
+  rm -f $PID_FILE
+  exit $exitVal
+}
+
+
+#====================================================================================================================-
+#--- Main script -----------------------------------------------------------------------------------------------------
+
+[ -e $PID_FILE ] && { echo "Instance already running, aborting another one" ; exit 1; } # run before installing traphandler!
+trap trapExitHandler SIGTERM EXIT
+
+echo "$$" > $PID_FILE
+
+[ -e $URL_FILE ] || { gRetMessage="Cannot read url file $URL_FILE" ; exit 1; }
+
+while IFS= read -r fhemUrl; do
+  fhemwebState=$( curl \
+                    --silent \
+                    --insecure \
+                    --output /dev/null \
+                    --write-out "%{http_code}" \
+                    --user-agent 'FHEM-Docker/1.0 Health Check' \
+                    "${fhemUrl}" )
+  if [ $? -ne 0 ] ||
+     [ -z "${fhemwebState}" ] ||
+     [ "${fhemwebState}" == "000" ] ||
+     [ "${fhemwebState:0:1}" == "5" ]; then
+    gRetMessage="$gRetMessage $fhemUrl: FAILED ($fhemwebState);"
+    gRetVal=1
+    ((gFailedCnt++))
+  else
+    gRetMessage="$gRetMessage $fhemUrl: OK;"
+    ((gSuccessCnt++))
+  fi
+done < $URL_FILE
+
+exit $gRetVal
+
+#!/bin/bash
+
 #--- Constants -------------------------------------------------------------------------------------------------------
 
 declare -r PID_FILE="/var/run/health-check.pid"
