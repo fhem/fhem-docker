27d9839e4bb3474f4b957f4567c5c6f6b16f145b
sidey79 <7968127+sidey79@users.noreply.github.com>
2024-04-02 20:33:23 +0200

    Default: gateway.docker.internal is not set Fixes #175

--- src/tests/bats/setup_suite.bash
+++ src/tests/bats/setup_suite.bash
@@ -1,6 +1,9 @@
 setup_suite() {
     export FHEM_DIR="/opt/fhem"
     export FHEM_CFG_FILE="${FHEM_DIR}/fhem.cfg"
+    export HOSTS_FILE='/etc/hosts'
+
+    cp ${HOSTS_FILE} ${BATS_SUITE_TMPDIR}/hosts 
     mkdir -p /tmp/fhem/FHEM
     cp -r /fhem/FHEM/*  /tmp/fhem/FHEM/
 
